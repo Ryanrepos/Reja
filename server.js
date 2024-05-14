8e010e96bb09cc0a7e85cd2ab83b25e4c7b5c4c9
ryanrepos <mamarasulov911@gmail.com>
2024-05-14 21:12:03 +0900

    EJS

--- server.js
+++ server.js
@@ -1,29 +1,43 @@
 console.log("Web serverni boshlash");
 
+const { log } = require("console");
 // 📌📌📌  EXPRESS  📌📌📌
 
 const express = require("express");
 const app = express();   // instance yasab olamz, express dan foydalanish uchun!
+const res = require("express/lib/response");
 const http = require("http");
 
 // 1: 📌 Intro code
 app.use(express.static("public"));
 app.use(express.json());    // serverdan kelayotdan datani json formatga o'tkazib beradi!
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));  // kelayotdan data ni parse qilib beradi!
 
 // 2: 📌 Session code
 
 // 3: 📌 Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
+app.set("views", "views");  // ikkinchida kelgan views bu folder nomi.
+app.set("view engine", "ejs");   // ejs ni ishlatyabmz view engine sifatida.
 
 // 4: 📌 Routing code
+
+/*
 app.get("/hello", function(req, res){
     res.end(`<h1 style = "background: red">HELLO WORLD by Ryan</h1>`); 
 });
 
 app.get("/gift", function(req, res){
     res.end("<h1>siz sovg'alar bo'limidasiz</h1>"); 
+});
+*/
+
+app.post("/create-item", (req, res) => {
+    console.log((req.body));
+    res.json({test: "success"});
+});
+
+app.get("/", function(req, res){
+    res.render("harid");
 });
 
 const server = http.createServer(app);
@@ -38,3 +52,6 @@ server.listen(PORT, function(){
 // 📌 Nodemon -- bu package kodda o'zgarish bo'lsa serverni qayta ishga tushiradi!
 // npm start => faqat start script bo'lsagina run beradi.
 // npm run start => start dan boshqa script lar bo'lsa masalan: dev, bu holatda npm run dev ishlatamz!
+
+// 📌 GET - database dan ma'lumotni olish uchun GET ishlatiladi.
+// 📌 POST - ma'lumotni o'zi bilan olib keladi va database ga o'sha ma'lumotni yozadi.
