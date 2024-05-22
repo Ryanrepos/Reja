ad20920c639e3bed61ed28aae231b2a25f9347c5
ryanrepos <mamarasulov911@gmail.com>
2024-05-23 03:53:20 +0900

    feat: connecting to MongoDB logic

---
+++ app.js
@@ -0,0 +1,74 @@
+console.log("Web serverni boshlash");
+
+const { log } = require("console");
+// 📌📌📌  EXPRESS  📌📌📌
+
+const express = require("express");
+const app = express();   // instance yasab olamz, express dan foydalanish uchun!
+const res = require("express/lib/response");
+
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    }else {
+        user = JSON.parse(data)
+    }
+});
+
+
+// MongoDB chaqirish..
+
+const mongodb = require("./server").db()
+
+
+// 1: 📌 Intro code
+app.use(express.static("public"));
+app.use(express.json());    // serverdan kelayotdan datani json formatga o'tkazib beradi!
+app.use(express.urlencoded({extended: true}));  // kelayotdan data ni parse qilib beradi!
+
+// 2: 📌 Session code
+
+// 3: 📌 Views code
+app.set("views", "views");  // ikkinchida kelgan views bu folder nomi.
+app.set("view engine", "ejs");   // ejs ni ishlatyabmz view engine sifatida.
+
+// 4: 📌 Routing code
+
+/*
+app.get("/hello", function(req, res){
+    res.end(`<h1 style = "background: red">HELLO WORLD by Ryan</h1>`); 
+});
+
+app.get("/gift", function(req, res){
+    res.end("<h1>siz sovg'alar bo'limidasiz</h1>"); 
+});
+*/
+
+app.post("/create-item", (req, res) => {
+    console.log((req.body));
+    res.json({test: "success"});
+});
+
+app.get("/", function(req, res){
+    res.render("reja");
+});
+
+app.get("/author", (req, res) => {
+    res.render("author", {user: user});
+})
+
+module.exports = app;
+
+
+// Yuqorida Express orqali web server qurib oldik!
+// EJS orqali backend ichida frontend ni qurib olamz!
+
+// 📌 Nodemon -- bu package kodda o'zgarish bo'lsa serverni qayta ishga tushiradi!
+// npm start => faqat start script bo'lsagina run beradi.
+// npm run start => start dan boshqa script lar bo'lsa masalan: dev, bu holatda npm run dev ishlatamz!
+
+// 📌 GET - database dan ma'lumotni olish uchun GET ishlatiladi.
+// 📌 POST - ma'lumotni o'zi bilan olib keladi va database ga o'sha ma'lumotni yozadi.
--- server.js
+++ server.js
@@ -1,72 +1,23 @@
-console.log("Web serverni boshlash");
 
-const { log } = require("console");
-// 📌📌📌  EXPRESS  📌📌📌
+const http = require("http");
+const mongodb = require("mongodb");
+const { error } = require("console");
 
-const express = require("express");
-const app     = express();   // instance yasab olamz, express dan foydalanish uchun!
-const res     = require("express/lib/response");
-const http    = require("http");
+let db;
+const connectionString = "mongodb+srv://Muhammadjon:muhammad06@cluster0.bxrbaic.mongodb.net/"
+mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true,}, (err, client) => {
+    if(err) console.log("ERROR on connection MongoDB");
+    else{
+    console.log("MongoDB connection succeed");
 
-const fs = require("fs");
+    module.exports = client;
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
-    }else {
-        user = JSON.parse(data)
+    const app = require("./app");
+    const server = http.createServer(app);
+    let PORT = 3000;
+    server.listen(PORT, function(){
+    console.log(`server is successfully running on port: ${PORT}, http://localhost:${PORT}`);
+})
     }
 });
 
-// 1: 📌 Intro code
-app.use(express.static("public"));
-app.use(express.json());    // serverdan kelayotdan datani json formatga o'tkazib beradi!
-app.use(express.urlencoded({extended: true}));  // kelayotdan data ni parse qilib beradi!
-
-// 2: 📌 Session code
-
-// 3: 📌 Views code
-app.set("views", "views");  // ikkinchida kelgan views bu folder nomi.
-app.set("view engine", "ejs");   // ejs ni ishlatyabmz view engine sifatida.
-
-// 4: 📌 Routing code
-
-/*
-app.get("/hello", function(req, res){
-    res.end(`<h1 style = "background: red">HELLO WORLD by Ryan</h1>`); 
-});
-
-app.get("/gift", function(req, res){
-    res.end("<h1>siz sovg'alar bo'limidasiz</h1>"); 
-});
-*/
-
-app.post("/create-item", (req, res) => {
-    console.log((req.body));
-    res.json({test: "success"});
-});
-
-app.get("/", function(req, res){
-    res.render("reja");
-});
-
-app.get("/author", (req, res) => {
-    res.render("author", {user: user});
-})
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function(){
-    console.log(`server is successfully running on port: ${PORT}, http://localhost:${PORT}`);
-});
-
-// Yuqorida Express orqali web server qurib oldik!
-// EJS orqali backend ichida frontend ni qurib olamz!
-
-// 📌 Nodemon -- bu package kodda o'zgarish bo'lsa serverni qayta ishga tushiradi!
-// npm start => faqat start script bo'lsagina run beradi.
-// npm run start => start dan boshqa script lar bo'lsa masalan: dev, bu holatda npm run dev ishlatamz!
-
-// 📌 GET - database dan ma'lumotni olish uchun GET ishlatiladi.
-// 📌 POST - ma'lumotni o'zi bilan olib keladi va database ga o'sha ma'lumotni yozadi.
